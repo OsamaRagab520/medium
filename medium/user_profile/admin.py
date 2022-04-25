6887c756373126a7c50f4ecf0bd3023ecdc73278
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2022-04-25 16:18:43 +0200

    add Profile models to amdin panel

--- medium/user_profile/admin.py
+++ medium/user_profile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# lOCAL IMPORTS GOES HERE!
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user','created_at')
+    list_filter = ('created_at', 'user')
+
+
