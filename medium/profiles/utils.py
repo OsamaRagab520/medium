cc2c0e69126598c44359c7be3dc8ad84a3ef59bb
Mohamad Aboda <41721894+Mohamad-Aboda@users.noreply.github.com>
2022-04-29 15:57:02 +0200

    MCB-37 (#19)
    * update gitignore to hold Pipfile
    
    * Add supseruser command
    
    * add profiles to installed apps
    
    * implement the profiles model
    
    * add resize func to resize images and helper func to help organize store path for media files
    
    * add profiles to the admin panel
    
    * update app name to match the one in installed apps

---
+++ medium/profiles/utils.py
@@ -0,0 +1,28 @@
+import uuid
+from io import BytesIO
+
+from django.core.files import File
+from django.core.files.base import ContentFile
+from django.db import models
+from PIL import Image
+
+
+def profile_pic_url(instance, filename):
+    return f"profile_images/{instance.user}/{filename}"
+
+
+def header_pic_url(instance, filename):
+    return f"header_images/{instance.user}/{filename}"
+
+
+def resize(imageField: models.ImageField, size: tuple):
+    im = Image.open(imageField)
+    source_image = im.convert("RGB")
+    source_image.thumbnail(size)
+    output = BytesIO()
+    source_image.save(output, format="JPEG")
+    output.seek(0)
+    content_file = ContentFile(output.read())
+    file = File(content_file)
+    random_name = f"{uuid.uuid4()}.jpeg"
+    imageField.save(random_name, file, save=False)
