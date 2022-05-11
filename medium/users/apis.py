ff6b1780d931594ba00856193f64ee3ec8bf4f33
Osama Ragab <osamaragab520@gmail.com>
2022-05-11 16:28:44 +0200

    feat: write create, update and retrieve endpoints for users

---
+++ medium/users/apis.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers, status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# from medium.api.permissions import IsOwner
+from medium.users.selectors import get_user
+from medium.users.services import UserService
+
+
+class CreateUserApi(APIView):
+    permission_classes = [AllowAny]
+
+    class InputSerializer(serializers.Serializer):
+        username = serializers.CharField()
+        password = serializers.CharField()
+        name = serializers.CharField()
+        email = serializers.EmailField()
+
+    def post(self, request):
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        service = UserService()
+        service.create_user(**serializer.validated_data)
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class DetailUserApi(APIView):
+    # TODO Remove when JWT authentication is implemented
+    permission_classes = [AllowAny]
+
+    class OutputSerializer(serializers.Serializer):
+        username = serializers.CharField()
+        name = serializers.CharField()
+        email = serializers.EmailField()
+
+    def get(self, request, user_id):
+        user = get_user(user_id)
+        serializer = self.OutputSerializer(user)
+        return Response(serializer.data)
+
+
+class UpdateUserApi(APIView):
+
+    # TODO Remove AllowAny and activate IsOwner when JWT authentication is implemented
+    permission_classes = [AllowAny]
+    # permission_classes = [IsOwner]
+
+    class InputSerializer(serializers.Serializer):
+        name = serializers.CharField(required=False)
+        email = serializers.EmailField(required=False)
+        password = serializers.CharField(required=False)
+
+    def post(self, request, user_id):
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        service = UserService()
+        service.update_user(
+            user_id=user_id,
+            data=serializer.validated_data,
+        )
+        return Response(status=status.HTTP_200_OK)
--- medium/users/tests/test_drf_urls.py
+++ medium/users/tests/test_drf_urls.py
@@ -8,17 +8,17 @@ pytestmark = pytest.mark.django_db
 
 def test_user_detail(user: User):
     assert (
-        reverse("api:user-detail", kwargs={"username": user.username})
-        == f"/api/users/{user.username}/"
+        reverse("api:users:get_user", kwargs={"username": user.pk})
+        == f"/api/users/{user.pk}/"
     )
-    assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
+    assert resolve(f"/api/users/{user.pk}/").view_name == "api:users:get_user"
 
 
-def test_user_list():
-    assert reverse("api:user-list") == "/api/users/"
-    assert resolve("/api/users/").view_name == "api:user-list"
+def test_user_create():
+    assert reverse("api:users:create_user") == "/api/users/me/"
+    assert resolve("/api/users/me/").view_name == "api:users:create_user"
 
 
-def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
-    assert resolve("/api/users/me/").view_name == "api:user-me"
+def test_user_update():
+    assert reverse("api:users:update_user") == "/api/users/me/"
+    assert resolve("/api/users/me/").view_name == "api:users:update_user"
